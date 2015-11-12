e34d72cbd324aa608d1d6836e121e4e5a985dc62
Redsart <redsartvimes@yahoo.com>
2015-11-12 18:55:06 +0200

    Added Dependencies Resolving problem

---
+++ DependenciesResolving/DependenciesResolving.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DependenciesResolving
+{
+    class DependenciesResolving
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader reader = new StreamReader(@"..\..\dependencies.json");
+
+            //using (reader)
+            //{
+            //    string textLine = reader.ReadLine();
+            //    while (textLine != null)
+            //    {
+            //        textLine = reader.ReadLine();
+            //    }
+            //}
+            string path = @"..\..\dependencies.json";
+            string content = File.ReadAllText(path);
+            Console.WriteLine(content);
+        }
+    }
+}
